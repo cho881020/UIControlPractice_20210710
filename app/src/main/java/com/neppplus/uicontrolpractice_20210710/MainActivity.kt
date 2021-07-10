0bfa13dcdc55b27fec043b4bf2cc7e190bb949fa
조경진 <cho881020@gmail.com>
2021-07-10 12:12:21 +0900

    버튼 누르면 -> 텍스트뷰에 입력한 내용 복붙

--- app/src/main/java/com/neppplus/uicontrolpractice_20210710/MainActivity.kt
+++ app/src/main/java/com/neppplus/uicontrolpractice_20210710/MainActivity.kt
@@ -2,14 +2,17 @@ package com.neppplus.uicontrolpractice_20210710
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
 //        토스트버튼이 눌리면 (이벤트 처리)
         toastBtn.setOnClickListener {
@@ -20,6 +23,11 @@ class MainActivity : AppCompatActivity() {
 
 //            토스트의 문구로 사용. (기록해둔 값 사용)
             Toast.makeText(this, inputContent, Toast.LENGTH_SHORT).show()
+
+
+//            resultTxt 의 text속성에, inputContent의 내용을 넣어주자.
+
+            resultTxt.text = inputContent
 
         }
 
